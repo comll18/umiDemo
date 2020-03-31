b81276ef695a327ddbc9bd7dcd1c213544baf7e6
comll18 <comll18@126.com>
2020-03-31 17:57:45 +0800

    路由及权限

---
+++ .umirc.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  dynamicImport: {}, //开启按需加载，umi路由会自动分割文件
+  routes: [
+    { path: '/', component: '@/pages/index/index' },
+    { path: '/user', component: '@/pages/user/index' },
+  ],
+});
